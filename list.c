f7a4cdc4414b00e2c6c9ce4172967b1ac5b4d7a1
jmount15 <jmount@umich.edu>
2016-01-26 12:18:41 -0500

    Update list.c

--- list.c
+++ list.c
@@ -10,6 +10,22 @@
 list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
+	
+	list_t* editor = head;
+	list_t* lastword = NULL;
+	//If new_element should be first
+	if (head->index > new_element->index){
+		new_element->next = head;
+		return new_element;
+	}
+	//Find when new_element should be placed
+	while((editor->index < new_element->index) && (editor != NULL)){
+		lastword = editor;
+		editor = editor->next;
+	}
+	
+	lastword->next = new_element;
+	new_element->next = editor;
 
 	return head;
 }
@@ -18,7 +34,15 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
-
-	return head;
+	//Initialize lists
+	list_t * a = head;
+	list_t * b = NULL;
+	list_t * c = NULL;
+	while (a != NULL) {
+		c = a->next;		//Hold location of next list
+		a->next=b;		//Change the next
+		b = a;			//Move location of previous location (next branch)
+		a = c;			//move a to old next
+	}
+	return b;
 }
-
